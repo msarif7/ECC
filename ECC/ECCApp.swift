2469f771e2b97a51cf951992848cbbbd39605619
Mustafa <mustafa_sarif@hotmail.com>
2023-03-23 14:21:07 -0500

    Initial Commit

---
+++ ECC/ECCApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECCApp.swift
+//  ECC
+//
+//  Created by MSA Software on 3/23/23.
+//
+
+import SwiftUI
+
+@main
+struct ECCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
